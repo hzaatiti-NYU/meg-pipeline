c25f135d590401dd7ace0e8b462bacb89f0d8cf0
hzaatiti <hadizaatiti@gmail.com>
2024-01-25 15:42:48 +0400

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'MEGPipeline'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MEG Pipeline'
+copyright = '2023, Hadi Zaatiti'
+author = 'Hadi Zaatiti'
 
 release = '0.1'
 version = '0.1.0'
