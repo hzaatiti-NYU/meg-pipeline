c0a93b9df9043409f994dbd4a640f0c726f25cd8
hzaatiti <hadizaatiti@gmail.com>
2024-03-26 09:24:59 +0400

    add resting state

---
+++ experiments/general/button_response.m
@@ -0,0 +1,19 @@
+%% Experiment for testing/template for response buttons
+
+
+clearvars
+%Screen('Preference', 'SkipSyncTests', 0);
+AssertOpenGL;  
+
+
+vpix_use = 1; % 0 if vpixx is not conected
+
+
+
+% KEYBOARD SETUP
+responseKeys = {'1', '2', '3', '4', '5'};
+KbName('UnifyKeyNames');  
+KbCheckList = [KbName('space'),KbName('ESCAPE'), KbName('leftarrow'), KbName('rightarrow')];
+for i = 1:length(responseKeys)
+    KbCheckList = [KbName(responseKeys{i}),KbCheckList];
+end
